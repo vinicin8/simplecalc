c7906ec9be00780d8851b2cdaca34f5b5a89d85c
Vinicius Ricardo <vinicius.ricardo@escolar.ifrn.edu.br>
2020-02-11 16:05:49 -0300

    100% work

--- src/calculadora/sceneController.java
+++ src/calculadora/sceneController.java
@@ -20,13 +20,13 @@ import javafx.scene.control.TextField;
  * @author 20191148060007
  */
 public class sceneController implements Initializable {
-    
+
     double valor1 = 0;
     double valor2 = 0;
     double resultado = 0;
-    
+
     String operacao = "";
-    
+
     @FXML
     private Label label;
     @FXML
@@ -65,89 +65,161 @@ public class sceneController implements Initializable {
     private Button dividirButton;
     @FXML
     private Button apagarButton;
-    
-  
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-      um.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "1");
-          }
-      });
-      
-      dois.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "2");
-          }
-      });
-      
-      tres.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "3");
-          }
-      });
-      
-      quatro.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "4");
-          }
-      });
-      
-      cinco.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "5");
-          }
-      });
-      
-      seis.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "6");
-          }
-      });
-      
-      sete.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "7");
-          }
-      });
-      
-      oito.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "8");
-          }
-      });
-      
-      nove.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "9");
-          }
-      });
-      
-      zero.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-              telaTextField.setText(telaTextField.getText()+ "0");
-          }
-      });
-      
-      multiButton.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-            valor1 = Double.parseDouble(telaTextField.getText());
-          }
-      });
-              
-    }    
-    
-    
-    
+        um.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "1");
+            }
+        });
+
+        dois.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "2");
+            }
+        });
+
+        tres.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "3");
+            }
+        });
+
+        quatro.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "4");
+            }
+        });
+
+        cinco.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "5");
+            }
+        });
+
+        seis.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "6");
+            }
+        });
+
+        sete.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "7");
+            }
+        });
+
+        oito.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "8");
+            }
+        });
+
+        nove.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "9");
+            }
+        });
+
+        zero.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + "0");
+            }
+        });
+
+        multiButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                valor1 = Double.parseDouble(telaTextField.getText());
+                telaTextField.setText("");
+                operacao = "*";
+            }
+
+        });
+        
+        dividirButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                valor1 = Double.parseDouble(telaTextField.getText());
+                telaTextField.setText("");
+                operacao = "/";
+            }
+        });
+        
+        somarButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                valor1 = Double.parseDouble(telaTextField.getText());
+                telaTextField.setText("");
+                operacao = "+";
+            }
+        });
+        
+        diminuirButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                valor1 = Double.parseDouble(telaTextField.getText());
+                telaTextField.setText("");
+                operacao = "-";
+            }
+        });
+        
+        apagarButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+               telaTextField.setText("");
+               valor1 = 0;
+               valor2= 0;
+               operacao="";
+            }
+        });
+        
+        igualButton.setOnAction (new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                valor2 = Double.parseDouble(telaTextField.getText());
+                
+                switch(operacao){
+                    case "+": 
+                        resultado = valor1 + valor2;
+                        telaTextField.setText(String.valueOf(resultado));
+                        break;
+                    case "-":
+                        resultado = valor1 - valor2;
+                        telaTextField.setText(String.valueOf(resultado));
+                        break;
+                    case "*":
+                        resultado = valor1 * valor2;
+                        telaTextField.setText(String.valueOf(resultado));
+                        break;
+                    case "/":
+                        resultado = valor1 / valor2;
+                        telaTextField.setText(String.valueOf(resultado));
+                        break;
+                }
+                operacao="";
+                valor1=0;
+                valor2=0;
+            }
+        });
+        
+        decimalButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                telaTextField.setText(telaTextField.getText() + ".");
+            }
+        });
+    }
+
 }
